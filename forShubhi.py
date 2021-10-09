5b8911378564620b3efd56e58bf917b7973ddc1c
naman2003now <69296186+naman2003now@users.noreply.github.com>
2021-10-09 09:15:38 +0530

    Update forShubhi.py
    added New features and Bug fixes

--- forShubhi.py
+++ forShubhi.py
@@ -1,23 +1,30 @@
 
+
 import pygame
 from pygame.locals import *
 import sys
 from time import sleep
+
+#bpVariables
 direction = "L"
+change = True
 
 # Colours
 SnakeColor = (250, 250, 255)
 BGColor = (50, 50, 50)
 FoodColor = (255, 60, 60)
 
-# pyGame Init
+#Global bp Variables
 windowHeight, windowWidth = 600, 600
+gridHeight, gridWidth = 20, 20
+
+# pyGame Init
 window = pygame.display.set_mode((windowHeight, windowWidth))
 pygame.display.set_caption("SnakeGame")
 
 def drawRect(Color, Coord):  # Draw Function
-    xCoord = Coord[0] * 20
-    yCoord = Coord[1] * 20
+    xCoord = Coord[0] * gridHeight
+    yCoord = Coord[1] * gridWidth
     pygame.draw.rect(window, Color, (yCoord, xCoord, 20, 20))
 
 def refreshScreen():
@@ -36,17 +43,21 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w and change:
                 if not direction == "D":
+                    change = False
                     direction = "U"
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s and change:
                 if not direction == "U":
+                    change = False
                     direction = "D"
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a and change:
                 if not direction == "R":
+                    change = False
                     direction = "L"
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d and change:
                 if not direction == "L":
+                    change = False
                     direction = "R"
     gameLoop()
     pygame.display.update()
